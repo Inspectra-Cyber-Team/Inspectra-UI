1ed97bfb847dc93a18175cc2f7cac5e2d15f4a3c
kdzy <kdeylester@gmail.com>
2024-12-10 20:16:56 +0700

    change my in button auth and move issue page into issueComponent

--- src/app/project/[projectName]/page.tsx
+++ src/app/project/[projectName]/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { navProjectDetial } from "@/data/navProjectDetial";
-import React from "react";
 
 export type ParamProps = {
   params: {
@@ -8,6 +7,8 @@ export type ParamProps = {
   };
 };
 
+import SecurityComponent from "@/components/securityComponent/SecurityComponent";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -19,11 +20,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Button } from "@/components/ui/button";
-import SecurityComponent from "@/components/securityComponent/SecurityComponent";
-import IssueSidebarComponent from "@/components/ProjectDetailComponents/IssueSidebarComponent";
-import IssueCardComponet from "@/components/ProjectDetailComponents/IssueCardComponet";
-
+import IusseComponent from "@/components/ProjectDetailComponents/IusseComponent";
 export default function page({ params }: ParamProps) {
   const projectName = params.projectName;
 
@@ -69,18 +66,7 @@ export default function page({ params }: ParamProps) {
           </Card>
         </TabsContent>
         <TabsContent value="Issue">
-          <section className="w-full h-full flex justify-between">
-            <div className="w-[35%] h-full bg-background_light_mode dark:bg-background_dark_mode p-5 rounded-[20px] hidden md:block ">
-              <p className="text-text_title_24 text-text_color_light dark:text-text_color_dark ">
-                Filter
-              </p>
-              <hr className="text-text_color_desc_light mt-2" />
-              <IssueSidebarComponent props={projectName} />
-            </div>
-            <div className="w-[60%] ">
-              <IssueCardComponet props={projectName} />
-            </div>
-          </section>
+            <IusseComponent props={projectName}/>
         </TabsContent>
 
         <TabsContent value="Security Hotspot">
--- src/redux/service/issue.ts
+++ src/redux/service/issue.ts
@@ -3,9 +3,9 @@ import { cyberApi } from "@/redux/api";
 export const issueApi = cyberApi.injectEndpoints({
   endpoints: (builder) => ({
     // get user feedback
-    getAllIssue: builder.query<any,{projectName: string, page: number, size: number }>({
-      query: ({projectName, page, size}) => ({
-        url: `/issues?projectName=${projectName}&page=${page}&size=${size}`,
+    getAllIssue: builder.query<any,{projectName: string, page: number, size: number, languages: string ,tags: string, files:string, directories: string, impactSeverities: string}>({
+      query: ({projectName, page, size, languages, tags, files, directories, impactSeverities}) => ({
+        url: `/issues?projectName=${projectName}&page=${page}&size=${size}&languages=${languages}&tags=${tags}&files=${files}&directories=${directories}&impactSeverities=${impactSeverities}`,
       }),
     }),
   }),
