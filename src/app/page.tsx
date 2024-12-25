297088b70e293b6f975b39d6e6f4c04566acec0a
HomPheakakvotey <voteyhom@gmail.com>
2024-12-25 14:35:45 +0700

    adjust style on myProfile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,43 +5,42 @@ import HeroComponent from "@/components/HeroComponent/HeroComponent";
 import HomepageComponent from "@/components/HomepageComponent.tsx/HomepageComponent";
 import { Metadata } from "next";
 
-export const metadata: Metadata ={
-  title: 'Inspectra',
+export const metadata: Metadata = {
+  title: "Inspectra",
   description:
-    'Welcome to Inspectra, a cutting-edge white-box testing platform from Cambodia that ensures secure development through source code analysis and identification of security vulnerabilities.',
+    "Welcome to Inspectra, a cutting-edge white-box testing platform from Cambodia that ensures secure development through source code analysis and identification of security vulnerabilities.",
   keywords:
-    'Inspectra homepage, Inspectra, inspectra, inspect, source code scan, scan, code scan, white-box testing Cambodia, secure development platform, source code analysis, cybersecurity tools, secure coding practices',
-  authors: { name: 'Inspectra Team' },
-  publisher: 'Inspectra',
+    "Inspectra homepage, Inspectra, inspectra, inspect, source code scan, scan, code scan, white-box testing Cambodia, secure development platform, source code analysis, cybersecurity tools, secure coding practices",
+  authors: { name: "Inspectra Team" },
+  publisher: "Inspectra",
   openGraph: {
-    title: 'Inspectra',
+    title: "Inspectra",
     description:
-      'Welcome to Inspectra, a cutting-edge white-box testing platform from Cambodia that ensures secure development through source code analysis and identification of security vulnerabilities.',
-    siteName: 'Inspectra',
-    locale: 'en_KH',
-    type: 'website',
-    url: 'https://inspectra.istad.co/',
+      "Welcome to Inspectra, a cutting-edge white-box testing platform from Cambodia that ensures secure development through source code analysis and identification of security vulnerabilities.",
+    siteName: "Inspectra",
+    locale: "en_KH",
+    type: "website",
+    url: "https://inspectra.istad.co/",
     images: [
       {
-        url: 'http://136.228.158.126:4011/images/3639a448-5eb8-43f4-bba9-a7f98c27792e.png',
+        url: "http://136.228.158.126:4011/images/3639a448-5eb8-43f4-bba9-a7f98c27792e.png",
         width: 1200,
         height: 630,
-        alt: 'Inspectra',
+        alt: "Inspectra",
       },
     ],
   },
-}
+};
 
 export default function Home() {
   return (
     <main className="w-[90%] mx-auto relative z-10">
-
       {/* Hero Section */}
       <HeroComponent />
 
       {/* Our Working Process */}
-     <HomepageComponent/>
-     
+      <HomepageComponent />
+
       {/* benefit section */}
       <section className="w-full text-center lg:my-[80px]">
         <BenefitComponentCard />
@@ -54,7 +53,7 @@ export default function Home() {
 
       {/* FAQs section */}
       <section className="w-full text-center my-[60px]">
-          <HomePageFAQComponent />
+        <HomePageFAQComponent />
       </section>
     </main>
   );
