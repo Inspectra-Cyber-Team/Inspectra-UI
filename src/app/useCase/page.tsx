9556898b71320a6056ed38ea4d7964b3e89682d6
Lyhou Phiv <lyhou282@gmail.com>
2024-12-26 18:27:11 +0700

    rename folder

--- src/app/useCase/page.tsx
+++ src/app/useCase/page.tsx
@@ -2,36 +2,37 @@ import { Metadata } from "next";
 import UseCaseComponent from "@/components/UseCaseComponent/UseCaseComponent";
 import React from "react";
 
-export const metadata: Metadata ={
-  title: 'Use Case - Inspectra',
+export const metadata: Metadata = {
+  title: "Use Case - Inspectra",
   description:
-    'Inspectra is design specifically for developers, offering tools to make code scannning and development more easier and secure',
-    keywords:
-    'Inspectra use case, use case, Inspectra, inspectra, inspect, cambodia, inspectra istad, inspectra.istad, istad, source code scan, scan, code scan, use case, white-box testing Cambodia, secure development platform, source code analysis, cybersecurity tools, secure coding practices',
-  authors: { name: 'Inspectra Team' },
-  publisher: 'Inspectra',
+    "Inspectra is design specifically for developers, offering tools to make code scannning and development more easier and secure",
+  keywords:
+    "Inspectra use case, use case, Inspectra, inspectra, inspect, cambodia, inspectra istad, inspectra.istad, istad, source code scan, scan, code scan, use case, white-box testing Cambodia, secure development platform, source code analysis, cybersecurity tools, secure coding practices",
+  authors: { name: "Inspectra Team" },
+  publisher: "Inspectra",
   openGraph: {
-    title: 'Use Case - Inspectra',
+    title: "Use Case - Inspectra",
     description:
-      'Inspectra is design specifically for developers, offering tools to make code scannning and development more easier and secure',
-    siteName: 'Inspectra',
-    locale: 'en_KH',
-    type: 'website',
-    url: 'https://inspectra.istad.co/',
+      "Inspectra is design specifically for developers, offering tools to make code scannning and development more easier and secure",
+    siteName: "Inspectra",
+    locale: "en_KH",
+    type: "website",
+    url: "https://inspectra.istad.co/",
     images: [
       {
-        url: 'http://136.228.158.126:4011/images/3639a448-5eb8-43f4-bba9-a7f98c27792e.png',
+        url: "http://136.228.158.126:4011/images/3639a448-5eb8-43f4-bba9-a7f98c27792e.png",
         width: 1200,
         height: 630,
-        alt: 'Inspectra',
+        alt: "Inspectra",
       },
     ],
   },
-}
+};
 
 export default function page() {
   return (
     <main>
+      {/*  */}
       <UseCaseComponent />
     </main>
   );
